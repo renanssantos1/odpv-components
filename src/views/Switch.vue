6c1682de51ba5119a38bad31babcab6a457154c3
Renan <renan.santos@justdigital.com.br>
2020-08-03 16:06:46 -0300

    add to new component switch

---
+++ src/views/Switch.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <label class="odpv switch--container" for="switch">
+      <input id="switch" class="odpv switch" type="checkbox" />
+      <span>aceito os termos</span>
+    </label>
+
+    <label class="odpv switch--container" for="switch-facebook">
+      <input id="switch-facebook" class="odpv switch switch--facebook" type="checkbox" />
+      <span>aceito os termos</span>
+    </label>
+
+    <label class="odpv switch--container" for="switch-primary">
+      <input id="switch-primary" class="odpv switch switch--primary" type="checkbox" />
+      <span>aceito os termos</span>
+    </label>
+
+    <label class="odpv switch--container" for="switch-secondary">
+      <input id="switch-secondary" class="odpv switch switch--secondary" type="checkbox" />
+      <span>aceito os termos</span>
+    </label>
+
+    <label class="odpv switch--container" for="switch-black">
+      <input id="switch-black" class="odpv switch switch--black" type="checkbox" />
+      <span>aceito os termos</span>
+    </label>
+
+    <input id="switch-orange" class="odpv switch switch--orange" type="checkbox" />
+  </section>
+</template>
