2294dc2f86195c3b42dad3837575274c0f68c844
Renan <renan.santos@justdigital.com.br>
2020-08-14 12:55:36 -0300

    manutencao para as fontes

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -11,15 +11,19 @@
       />
 
       <div class="aph form__helper form__helper--error">
-        This field is required and
-        need a valid e-mail address
+        This field is required and need a valid e-mail address
       </div>
     </div>
 
-    <div>
-      <label class="odpv form__label">Name</label>
-      <input class="odpv form__control" type="text" disabled placeholder="Name" />
-    </div>
+    <label class="odpv form__label">Name</label>
+    <input class="odpv form__control" type="text" disabled placeholder="Name" />
+
+    <input type="checkbox" class="odpv form__control" />
+    <label class="odpv form__label" for="aceito-termos">
+      Aceito os termos
+    </label>
+
+    <i class="fas fa-check"></i>
   </section>
 </template>
 
@@ -27,5 +31,4 @@
 export default {};
 </script>
 
-<style>
-</style>
+<style></style>
